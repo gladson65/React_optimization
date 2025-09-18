9311d07ecf527fc89228f7c56a2060d21b5fbc52
gladson65 <grauth65@gmail.com>
2025-09-18 16:02:38 +0530

    useMemo is added on items array

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,14 @@ function App() {
   const [search, setSearch] = useState("");
   const [count, setCount] = useState(0);
 
-  const item = ["apple", "banana", "grapes", "guava", "cherry", "mango"];
+  const items = ["apple", "banana", "grapes", "guava", "cherry", "mango"];
+
+  // useMemo for avoid recalculating filtered list on every render
+  const filteredItems = useMemo(()=> {
+    return items.filter((item) => {
+      item.toLowerCase().includes(search.toLowerCase());
+    });
+  }, [search, items]);
 
   return (
     <>
