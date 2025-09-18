20a6b4d7284f2e4460d3091af1fb807c2db3cdb8
gladson65 <grauth65@gmail.com>
2025-09-18 15:59:04 +0530

    child component 'ListItems' is added and imported in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-
+import { useState, useMemo, useCallback } from 'react';
+import ListItems from './components/ListItems';
 import './App.css'
 
 function App() {
   
+  const [search, setSearch] = useState("");
+  const [count, setCount] = useState(0);
+
+  const item = ["apple", "banana", "grapes", "guava", "cherry", "mango"];
 
   return (
     <>
---
+++ src/components/ListItems.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// child component that only re-renders when props change
+function ListItems() {
+    React.memo(({ item, onClick}) => {
+        console.log('Render:', item);
+
+        return (
+            <li onClick={()=> onClick(item)} className="cursor-pointer hover:text-blue-500">
+                {item}
+            </li>
+        )
+    })
+}
+
+export default ListItems;
